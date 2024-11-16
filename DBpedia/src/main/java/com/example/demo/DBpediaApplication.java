c19576df0db0508ef9e85c654c3206f20cb860d0
dimsth <dimo35xf@gmail.com>
2024-11-16 15:20:30 +0200

    DBpedia start

---
+++ DBpedia/src/main/java/com/example/demo/DBpediaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBpediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DBpediaApplication.class, args);
+	}
+
+}
